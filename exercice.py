ed5a5ec106821828460054243a9a6b2f6389d76f
GabrielBruno24 <gabrielbruno24@gmail.com>
2020-09-14 22:42:02 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -30,7 +30,7 @@ def to_degrees(angle_rads: float) -> tuple:
     angle_degs = math.floor(degs)
     mins = (degs - angle_degs)*60
     angle_mins = math.floor(mins)
-    
+    secs = 
     return 0.0, 0.0, 0.0
 
 
